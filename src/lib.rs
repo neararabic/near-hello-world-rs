0cb9e46ef72483057f2371233dcd3dda5d2cc1c9
mohammed saudi <m.hassan@ogr.iu.edu.tr>
2022-03-07 10:50:39 +0300

    A working hello_world example

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+//نحتاج استيراد التالي حتى يمكن لبيئة العمل ترجمة السطور التالية و تحويلها لكود يعمل على البلوك تشين
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::near_bindgen;
+
+near_sdk::setup_alloc!();
+//ماكرو حتى يمكن لبية العمل ترجمة الكود التالي الى كود يعمل على البلوك تشين
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+
+//مكون الرئيسي للعقد الذكي - ستركتشر
+pub struct Contract {
+  //يمكن تعريف المتغيرات هنا
+}
+
+//الدوال الملحقة بالاستركتشر السابق تعريفه
+#[near_bindgen]
+impl Contract {
+  //تعريف الدوال هنا
+  pub fn hello_world(&self) -> std::string::String {
+    return "Hello from Rust!".to_string();
+  }
+}
